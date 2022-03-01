386491e91c8510ce5e4805caed32a551bd83bc1c
RSMP <rubshan.3121@gmail.com>
2022-03-01 07:52:58 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import styles from "../styles/Home.module.css";
+import requests from "../utils/requests";
+
+export default function Home({ results }) {
+  return (
+    <div>
+      <Head>
+        <title>HULU-2.0</title>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+      {/* Nav */}
+      <Nav />
+      {/* Resultes */}
+      <Results results={results} />
+    </div>
+  );
+}
+// server rendering
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
